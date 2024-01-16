8413e780bb292f69bd8a6e9eaff89f8e8ebffd79
Dennis Guan <i0fear@yahoo.com>
2024-01-16 15:35:56 -0500

    add modal and close

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let employee =[];
+let employees = [];
 const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
 email, location, phone, dob &noinfo &nat=US`;
 const gridContainer =document.querySelector(".grid-container");
@@ -16,7 +16,7 @@ fetch(urlAPI)
 
 
 function displayEmployees(employeeData){
-    let employees =employeeData;
+     employees =employeeData;
 
     //store the employee HTML as we create it
     let employeeHTML ='';
@@ -48,36 +48,37 @@ function displayEmployees(employeeData){
 function displayModal(index) {
     // use object destructuring make our template literal cleaner
     let { name, dob, phone, email, location: { city, street, state, postcode
-    }, picture } = employee[index];
+    }, picture } = employees[index];
     let date = new Date(dob.date);
     const modalHTML = `
-    <img class="avatar" src="${picture.large}"/>
-    <div class="text-container">
-    <h2 class="name">${name.first} ${name.last} </h2>
-    <p class="email"> ${email}</p>
-    <p class="address"> ${city}</p>
-    <hr />
-    <p>${phone}</p>
-    <p class="address">${street}, ${state} ${postcode}</p>
-    <p>Birthday:
-    ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
-    </div>
+        <img class="avatar" src="${picture.large}" />
+        <div class="text-container">
+        <h2 class="name">${name.first} ${name.last}</h2>
+        <p class="email">${email}</p>
+        <hr />
+        <p class="tel">${phone}</p>
+        <p class="address">${street.number} ${street.name} <br> ${city}, ${state} ${postcode}</p>
+        <p class="birthday">Birthday:
+        ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+        </div> 
+        
     `;
     overlay.classList.remove("hidden");
     modalContainer.innerHTML = modalHTML;
-}    
-
-gridContainer.addEventListener('click', e => {
-    // make sure the click is not on the gridContainer itself
-    if (e.target !== gridContainer) {
-    // select the card element based on its proximity to actual element clicked
-    const card = e.target.closest(".card");
-    const index = card.getAttribute('data-index');
-    displayModal(index);
     }
-    });
 
-    modalClose.addEventListener('click', () => {
-        overlay.classList.add("hidden");
+    gridContainer.addEventListener('click', e => {
+        // make sure the click is not on the gridContainer itself
+        if (e.target !== gridContainer) {
+        // select the card element based on its proximity to actual element clicked
+        const card = e.target.closest(".card");
+        const index = card.getAttribute('data-index');
+        displayModal(index);
+        }
         });
+
+        modalClose.addEventListener('click', () => {
+            overlay.classList.add("hidden");
+            modalContainer.innerHTML = ""; // Clear the modal content
+          });
    
